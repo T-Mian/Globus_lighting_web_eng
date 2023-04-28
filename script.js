978726299b83279a6c878b176098dd8cb997a1af
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-04-28 09:48:26 +0000

    Nowe funkcje na scroll
    Teraz dwie funkcjie  onscroll działają jednocześnie
    Mega menu
    On top

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let mybutton = document.getElementById("myBtn");
+
+window.addEventListener("scroll", function() {
+  myEventHandlers.myFunction();
+});
+
+window.addEventListener("scroll", function() {
+  myEventHandlers.scrollFunction();
+});
+
+
+const myEventHandlers = {
+  scrollFunction: function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+},
+
+  myFunction: function () {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
