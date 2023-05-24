f971fa5624bc94f3b5b8d7ac9d68c8ffefc3ce7d
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-05-24 11:40:42 +0000

    element_menu na trafił na problem ,
     wiec po zapisaniu go "dla potomności", w el-menu, i uprościłem do maksimum ten proces,
    innymi słowy to będzie problem na później

--- element_menu.js
+++ element_menu.js
@@ -2,40 +2,14 @@
 //versja 1
 
 
-const caret_down = "<i class=\"fa fa-caret-down\"></i>"
+const logo = '<div><a href="index.html"><img id="logos" style="padding:25px;" class="logo_s hexagon_loga" src="svg/logo_Globus_lighting_FECB00.svg" onmouseover="this.src=\'svg/logo_Globus_lighting.svg\'" onmouseout="this.src=\'svg/logo_Globus_lighting_FECB00.svg\'"></a></div>'
 
-const caret_right= "<i class=\"fa-solid fa-caret-right\"></i>"
+document.getElementById("logo_firmy").innerHTML=logo
 
-const list_glowne_elementy=["Home1","Products2","Solutions2","Financing1","Realisations2","Blog1","About us2","Contact1"] 
 
-const lista_pod_menu=["LED products0","Industrial photovoltaics0","Car chargers0","LED products catalogue0","Industrial lighting0","Street lighting0","Hotel lighting0","Office lighting0","Commercial facilities lighting0","Sports facilities lighting0","Architectural lighting3","Monuments lighting0","Facade lighting0","Park ang garden lighting0","Parking lighting0","Workshop lighting0","Explosion proof lighting0","Lighting audit0","Main Railway Station in Wroclaw0","Mondelez0","Siegienia Aubi0","Stradom0"," Orlik football field0"," Glassworks lighting0","Production halls lighting0","Opakofarb0","City Forum Archicom0","LG Chem Magazine0","Lorenz Bahlsen Magazine0","Career0"]
 
-const lista_ikons_glowne=["<i class=\"fa-solid fa-house\"></i>","<i class=\"fa-solid fa-tags\"></i>","<i class=\"fa-solid fa-pen-ruler\"></i>","<i class=\"fa-solid fa-dollar-sign\"></i>","<i class=\"fa-solid fa-landmark-dome\"></i>","<i class=\"fa-solid fa-blog\"></i>"," <i class=\"fa-solid fa-passport\"></i>","<i class=\"fa-solid fa-pen-nib\"></i>"]
+let nav_list_text='<!--Navigation--><nav><ul class="menu" style="margin-left:10%;"><li><a href="home.html"><i class="fa-solid fa-house"></i>Home</a></li><li class="dropdown_menu"><a href="products_led.html"><i class="fa-solid fa-tags"></i><span>Products</span><i class="fa fa-caret-down"></i></a><div class="dropdown-content"><a href="#">LED products</a><a href="#">Industrial photovoltaics</a><a href="#">Car chargers</a><a href="#">LED products catalogue</a></div></li><li class="dropdown_menu"><a href="#"><i class="fa-solid fa-pen-ruler"></i><span>Solutions</span><i class="fa fa-caret-down"></i></a><div class="dropdown-content"><a href="#">Industrial lighting</a><a href="#">Street lighting</a><a href="#">Hotel lighting</a><a href="#"> Office lighting </a><a href="#"> Commercial facilities lighting </a><a href="#"> Sports facilities lighting </a><span class="side_menu"><a href="#">Architectural lighting<i class="fa-solid fa-caret-right"></i><div class="side-content"><a href="#">Monuments lighting</a><a href="#">Facade lighting</a><a href="#">Park ang garden lighting</a><a href="#"> Parking lighting</a></div></a></span><a href="#"> Workshop lighting </a><a href="#"> Explosion proof lighting </a><a href="#"> Lighting audit </a></div></li><li><a href="#"><i class="fa-solid fa-dollar-sign"></i>Financing</a></li><li class="dropdown_menu"><a href="#"><i class="fa-solid fa-landmark-dome"></i><span>Realisations</span><i class="fa fa-caret-down"></i></a><div class="dropdown-content"><a href="#">Main Railway Station in Wroclaw</a><a href="#">Mondelez</a><a href="#"> Siegienia Aubi </a><a href="#"> Stradom </a><a href="#"> Orlik football field </a><a href="#"> Glassworks lighting </a><a href="#"> Production halls lighting </a><a href="#"> Opakofarb </a><a href="#">City Forum Archicom</a><a href="#">LG Chem Magazine</a><a href="#">Lorenz Bahlsen Magazine</a></div></li><li><a href="#"><i class="fa-solid fa-blog"></i>Blog</a></li><li class="dropdown_menu"><a href="#"><i class="fa-solid fa-passport"></i>About us<i class="fa fa-caret-down"></i></a><div class="dropdown-content"><a href="#">Career</a></div></li><li><a href="#"><i class="fa-solid fa-pen-nib"></i>Contact</a></li></ul></nav>'
 
-const lista_adresow = ['home.html','products_led.html','#']
+document.getElementById("div_menu").innerHTML=nav_list_text
 
-const logo = `
-<div>
-  <a href="index.html">
-    <img id="logos" style="padding:25px;" class="logo_s hexagon_loga"           src="svg/logo_Globus_lighting_FECB00.svg"
-    onmouseover="this.src='svg/logo_Globus_lighting.svg'"
-    onmouseout="this.src='svg/logo_Globus_lighting_FECB00.svg'">
-    </a>
-</div>
-`
-const nav_list_text= '<!--Navigation--> <nav>'
-nav_list_text+='<ul class="menu" style="margin-left:10%;">'
 
-list_glowne_elementy.forEach(funkcjaBudującaMenu)
-nav_list_text += "</ul>";
-
-function funkcjaBudującaMenu(value){
-  let nr= 0
-  let wynik = ''
-  if ( value.slice(-1) == 1){
-    let val =  value.slice(0,-1)
-    wynik+= '<li>'+'<a href='+'"#"'+'>'+lista_ikons_glowne[nr] + val+'</a>'+ '</li>'
-    nav_list_text+=wynik
-    nr+=1
-  }
-}
