3beb12d04b68fe4fe9edb2f13b3ef74a62152a22
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-05-24 06:58:39 +0000

    Poprawka wyglądu menu , start skryptu budującego Menu

--- element_menu.js
+++ element_menu.js
@@ -1,7 +1,15 @@
 //Tomasz Mianecki 
 //versja 1
 
+let caret_down = "<i class=\"fa fa-caret-down\"></i>"
 
+let caret_right= "<i class=\"fa-solid fa-caret-right\"></i>"
 
+const list_glowne_elementy=["Home","Products","Solutions","Financing","Realisations","Blog","About us","Contact"] 
 
-  
+const lista_pod_menu=["LED products","Industrial photovoltaics","Car chargers","LED products catalogue","Industrial lighting","Street lighting","Hotel lighting","Office lighting","Commercial facilities lighting","Sports facilities lighting","Architectural lighting","Monuments lighting","Facade lighting","Park ang garden lighting","Parking lighting","Workshop lighting ","Explosion proof lighting","Lighting audit","Main Railway Station in Wroclaw","Mondelez","Siegienia Aubi","Stradom"," Orlik football field"," Glassworks lighting","Production halls lighting","Opakofarb","City Forum Archicom","LG Chem Magazine","Lorenz Bahlsen Magazine","Career"]
+
+const lista_ikons_glowne=["<i class=\"fa-solid fa-house\"></i>","<i class=\"fa-solid fa-tags\"></i>","<i class=\"fa-solid fa-pen-ruler\"></i>","," <i class=\"fa-solid fa-dollar-sign\"></i>","<i class=\"fa-solid fa-landmark-dome\"></i>","<i class=\"fa-solid fa-blog\"></i>"," <i class=\"fa-solid fa-passport\"></i>","<i class=\"fa-solid fa-pen-nib\"></i>"]
+
+const lista_adresow = ["home.html","products_led.html","#"]
+
