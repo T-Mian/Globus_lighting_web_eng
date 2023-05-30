b6ba5bfdba0945fc1d1bf2543ffcf775d0358b64
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-05-30 12:57:50 +0000

    zapis danych

--- script_tworzenie_kart_produktu.js
+++ script_tworzenie_kart_produktu.js
@@ -1,8 +1,8 @@
 // Tomasz Mianecki
 // versja 1
 
-let lista_rodzin_produktu = []
+let lista_rodzin_produktu = ["Belus" , "SQR" , "HB-U" , "HB-U2"];
 let obecna_data =  new Date();
-let obecnie_rok = obecna_data.getFullYear()
-let obecnie_miesiac = obecna_data.getMonth()
-let obecnie_dzien = obecna_data.getDay()
+let obecnie_rok = obecna_data.getFullYear();
+let obecnie_miesiac = obecna_data.getMonth();
+let obecnie_dzien = obecna_data.getDay();
