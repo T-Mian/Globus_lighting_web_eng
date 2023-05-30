7a96622882a14cb355bbc00b0abc80a8bfdae3d7
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-05-30 12:45:44 +0000

    Start tworzenia skryptu który tworzyć będzie nowe karty produktu

---
+++ script_tworzenie_kart_produktu.js
@@ -0,0 +1,8 @@
+// Tomasz Mianecki
+// versja 1
+
+let lista_rodzin_produktu = []
+let obecna_data =  new Date();
+let obecnie_rok = obecna_data.getFullYear()
+let obecnie_miesiac = obecna_data.getMonth()
+let obecnie_dzien = obecna_data.getDay()
