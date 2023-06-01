52d6fc12af8b7eb3e445de61bba21a573e44a673
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-06-01 07:02:35 +0000

    Zapis dla funkcji tworzenia kart
    Funkcja działa ale dane zawarte w xml , albo w tytułach trzeba będzie zmienić
    Potrzeba tez rozróżnić grze gdzie karty maja trafiać

--- script_tworzenie_kart_produktu.js
+++ script_tworzenie_kart_produktu.js
@@ -7,7 +7,7 @@ let obecna_data =  new Date();
 let obecnie_rok = obecna_data.getFullYear();
 let obecnie_miesiac = obecna_data.getMonth();
 let obecnie_dzien = obecna_data.getDay();
-let flaga_ribbon = ["NEW","TOP","NON"];
+let flaga_ribbon = ["TOP","NEW","NON"];
 let color_ribbon = "#fecb00";
 let hiper_link = ["strona_produktu_led.html"];
 let lista_docelowa_id=["_industr_carts","_line_carts","_tepmer_carts","_specjal_carts","_ex_carts","_zewenetrz_carts","_sport_zew_carts","_ofice_carts","_agro_carts","_archit_carts"];
@@ -22,21 +22,60 @@ function loadDoc() {
  xhttp.send();
 }
 
+function flagowanie(ribbon){
+  let flaga=""
+  
+  switch(ribbon) {
+    case '1':
+      flaga = flaga_ribbon[0]
+      return ("<div class='ribbon up' style='--color: #fecb00;'><div class='content_ribon'>"+flaga+"</div> </div>")
+      break;
+    case '2':
+      flaga = flaga_ribbon[1]
+      return ("<div class='ribbon up' style='--color: #fecb00;'><div class='content_ribon'>"+flaga+"</div> </div>")
+      break;
+    case '3':
+      flaga = flaga_ribbon[2]
+      return (" <!-- brak ribbon -->")
+      break;
+  }
+ 
+}
+
 function funkcjiaOdczytuXML(xml){
-  let numer = 1
+  var numer = 1
   const xmlDoc = xml.responseXML;
   const x = xmlDoc.getElementsByTagName("CARTS");
-   let carta="<tr><th>RODZINA</th><th>ALT</th><th>CATEGORIA</th></tr>";;
+  let karty_out=""
+   
    for (let i = 0; i <x.length; i++){
-      carta += "<tr><td>" +
-    x[i].getElementsByTagName("RODZINA")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("ALT")[0].childNodes[0].nodeValue +
-    "</td><td>"+
-     x[i].getElementsByTagName("CATEGORIA")[0].childNodes[0].nodeValue +
-    "</td></tr>"
+     let numerId = x[i].getElementsByTagName("NUMER_ID")[0].childNodes[0].nodeValue;
+     let rodzina = x[i].getElementsByTagName("RODZINA")[0].childNodes[0].nodeValue;
+     let alt = x[i].getElementsByTagName("ALT")[0].childNodes[0].nodeValue ;
+     let categoria = x[i].getElementsByTagName("CATEGORIA")[0].childNodes[0].nodeValue;
+     let flaga = x[i].getElementsByTagName("FLAGA")[0].childNodes[0].nodeValue;
+     let stronaWeb = x[i].getElementsByTagName("STRONA_WEB")[0].childNodes[0].nodeValue;
+     let div_flaga = flagowanie(flaga)
+     
+     var carta='<!--'+numer+'--><div><a href="'+stronaWeb+'.html" alt=" ' +alt+'" target="_blank"><div class="card_produkt"><div class="wrapper_produkt">'+div_flaga+'<img src="/produkty_karty_covery/Produkt_cover_bg_hex_'+rodzina+'.jpg" class="cover-image_produkt" /></div><img src="/produkty_karty_title/'+rodzina+'-title.png" class="title_produkt" /> <img src="/produkty_karty_charakter/'+rodzina+'-char_simple.png" class="character_produkt" /></div></a></div>';
+     karty_out+=carta
+     numer++
    }
-  document.getElementById("demo").innerHTML = carta;
+   
+  document.getElementById("test").innerHTML = karty_out;
 }
 
 loadDoc()
+
+// <a href="strona_produktu_led.html" alt="nazwa_alt" target="_blank">
+//        <div class="card_produkt">
+//          <div class="wrapper_produkt">
+//            <div class="ribbon up" style="--color: #fecb00;">
+//              <div class="content_ribon">New</div>
+//            </div>
+//            <img src="/produkty_karty_covery/XXX.jpg" class="cover-image_produkt" />
+//          </div>
+//          <img src="/produkty_karty_title/XXX.png" class="title_produkt" />
+//          <img src="/produkty_karty_charakter/XXX.png" class="character_produkt" />
+//       </div>
+//      </a> 
